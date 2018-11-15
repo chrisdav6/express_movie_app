70471ffe63a6687598a91682ed567d76fe3c536c
Chris Davis <chris.davis5440@gmail.com>
2018-11-15 08:56:20 -0500

    using serve-favicon node module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const favicon = require("serve-favicon");
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -17,16 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use(favicon(path.join(__dirname, '/public/images', 'favicon.ico')))
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
