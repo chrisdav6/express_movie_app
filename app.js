893c20879ce8ff332dc51c5c45fee85d2edc8744
Chris Davis <chris.davis5440@gmail.com>
2018-11-15 09:07:36 -0500

    using helmet node module

--- app.js
+++ app.js
@@ -4,22 +4,36 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const favicon = require("serve-favicon");
+const helmet = require("helmet");
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+//Helmet
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Logger
 app.use(logger('dev'));
+
+//Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Cookie Parser
 app.use(cookieParser());
+
+//Static Folders
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
+//Favicon
 app.use(favicon(path.join(__dirname, '/public/images', 'favicon.ico')))
 
+//Router
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
